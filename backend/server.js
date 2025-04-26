66635cb5bd9c8154ea9ae54ef39dde5a11356d5f
Nana Mohamadi <adilnana78@gmail.com>
2025-04-26 07:08:59 +0000

    update matcher

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 import path from "path";
 //routes importation
 import authRoutes from "./routes/auth.route.js";
-import productRoutes from "./routes/product.route.js"
-import cartRoutes from "./routes/cart.route.js"
+import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
-import paymentsRoutes from "./routes/payment.route.js"
-import analyticsRoutes from "./routes/analytics.route.js"
+import paymentsRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -19,11 +19,11 @@ const app = express();
 //My port running
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
 //parser json the body of the request
 app.use(express.json({ limit: "10mb" })); // parse json from the request
-app.use(cookieParser()) //parse cookies from the request
+app.use(cookieParser()); //parse cookies from the request
 
 //routes
 app.use("/api/auth", authRoutes);
@@ -33,12 +33,12 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentsRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/dist")))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
-  })
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 }
 
 app.listen(5000, () => {
